3645666904295bab1d4024cbec83544ff31888a2
Alvaro <alvaroelena97@gmail.com>
2022-06-08 19:43:56 -0300

    First upload

---
+++ js/Alumno.js
@@ -0,0 +1,82 @@
+class Alumnos{
+    constructor(){
+        this.lista = [];
+    }
+    agregarAlumno(alumno)
+    {
+        this.lista.push(alumno);
+    }
+
+    eliminarAlumno(index)
+    {
+        this.lista.splice(index,1);
+    }
+
+    getLargo()
+    {
+        return this.lista.length;
+    }
+
+    getIndex(id)
+    {
+        let index;
+        for (index = 0; index < this.getLargo(); index++) {
+            if(this.lista[index].getId()==id)
+                return index;
+        }
+        return -1;
+    }
+}
+
+class Alumno{
+    constructor(nombre, apellido, notas,id){
+        this.id= id;
+        this.nombre= nombre;
+        this.apellido=apellido;
+        this.notas=notas;
+    }
+
+    getNombre()
+    {
+        return this.nombre;
+    }
+    getApellido()
+    {
+        return this.apellido;
+    }
+    getNota()
+    {
+        return this.notas;
+    }
+
+    getId()
+    {
+        return this.id;
+    }
+
+    setId(id)
+    {
+        if(id >0 && id!=NaN)
+            this.id = id;
+        else
+            this.id =0;
+    }
+
+    setNombre(nombre)
+    {
+        this.nombre = nombre;
+    }
+    setApellido(apellido)
+    {
+        this.apellido= apellido;
+    }
+    setNota(notas)
+    {
+        this.notas=notas;
+    }
+
+    getAlumno()
+    {
+        return   "Id: "+ this.getId() +" - Nombre: "+ this.getNombre() + " " + this.getApellido() + " - Nota: " + this.getNota() +  "  <br>  ";
+    }
+};
---
+++ js/index.js
@@ -0,0 +1,358 @@
+const alumnosArray = new Alumnos();
+let id =0;
+
+MostrarElementos(0);
+
+function AgregarAlumno()
+{
+    notas = parseInt(document.getElementById("inputText2").value);
+    nombre = document.getElementById("inputText").value;
+    apellido = document.getElementById("inputText1").value;
+
+    if(notas >0 && notas<11 && notas != NaN)
+    {
+        id++;
+        alumnosArray.agregarAlumno(new Alumno(nombre,apellido,notas,id))
+        alertify.notify('Se ingreso el alumno correctamente', 'success', 5, function(){  console.log('dismissed'); });
+        LimpiarCampos();
+    }
+    else 
+        alertify.alert('Error','La nota ingresada no es correcta');
+}
+
+function LimpiarCampos()
+{
+    document.getElementById("idAlumnoCampo").value= "";
+    document.getElementById("inputText").value= "";
+    document.getElementById("inputText1").value= "";
+    document.getElementById("inputText2").value = "";
+}
+
+/*
+function ValidarCampos()
+{
+    parseInt(document.getElementById("notaAlumno").value);
+    document.getElementById("nombreAlumno").value;
+    document.getElementById("apellidoAlumno").value;
+}
+
+*/
+
+function MostrarAlumnos()
+{
+    let alumnos="";
+    let index;
+    for (index = 0; index < alumnosArray.lista.length; index++) {
+        alumnos = alumnos + alumnosArray.lista[index].getAlumno();
+    }
+    return alumnos;
+}
+
+function MostrarAlumnosHtml()
+{
+    if(alumnosArray.getLargo()==0 )
+        return alertify.notify('No hay alumnos cargados', 'error', 5, function(){  console.log('dismissed'); });
+
+    document.getElementById("resultado").innerHTML = MostrarAlumnos();
+}
+
+function PromedioDeNotas()
+{
+    if(alumnosArray.getLargo()==0 )
+        return alertify.notify('No hay alumnos cargados', 'error', 5, function(){  console.log('dismissed'); });
+
+    let total=0;
+    for (let index = 0; index < alumnosArray.lista.length; index++) {
+        total+=alumnosArray.lista[index].notas;
+    }
+    if(total!=NaN || total!=0)
+        alertify.alert('Alumnos','El promedio de notas es: ' + (total/alumnosArray.lista.length).toFixed(2));
+    
+}
+
+function EncontrarPeorAlumno()
+{
+    if(alumnosArray.getLargo()==0 )
+        return alertify.notify('No hay alumnos cargados', 'error', 5, function(){  console.log('dismissed'); });
+
+    let minimo=11;
+    let indice=0;
+    for (let index = 0; index < alumnosArray.lista.length; index++) {
+        if (alumnosArray.lista[index].notas < minimo)
+        {
+            minimo = alumnosArray.lista[index].notas;
+            indice=index
+        }
+    }
+    alertify.alert('El peor alumno es:', alumnosArray.lista[indice].getAlumno());
+
+}
+
+function EncontrarMejorAlumno()
+{
+    if(alumnosArray.getLargo()==0 )
+        return alertify.notify('No hay alumnos cargados', 'error', 5, function(){  console.log('dismissed'); });
+
+    let maximo=0;
+    let indice=0;
+    for (let index = 0; index < alumnosArray.lista.length; index++) {
+        if (alumnosArray.lista[index].notas > maximo)
+        {
+            maximo = alumnosArray.lista[index].notas;
+            indice=index
+        }
+    }
+    alertify.alert('El mejor alumno es:', alumnosArray.lista[indice].getAlumno());
+}
+
+function HardcodeAlumnos() 
+{
+    let nombres = ["juan","jose","sebastian"];
+    let apellidos=["alvaro","perez","gomez"];
+    let notasAlumnos=[8,2,5];
+    let index;
+    for (index = 0; index < 3; index++) {
+        id++;
+        alumnosArray.agregarAlumno(new Alumno(nombres[index],apellidos[index],notasAlumnos[index],id));
+    }
+    if(index>0)
+        alertify.alert('Exito','Se ingresaron alumnos Hardcodeadeados ');
+}
+
+function GuardarAlumnos()
+{
+    const myJSON = JSON.stringify(alumnosArray);
+    localStorage.setItem("Alumnos",myJSON);
+    console.log(myJSON);
+    alertify.notify('Se guardo correctamente', 'success', 5, function(){  console.log('dismissed'); });
+}
+
+function CargarAlumnos()
+{
+    const arrayTest = JSON.parse(localStorage.getItem("Alumnos"));
+    for (let index = 0; index < arrayTest.lista.length; index++) {
+        id++;
+        alumnosArray.agregarAlumno(new Alumno(arrayTest.lista[index].nombre,arrayTest.lista[index].apellido,arrayTest.lista[index].notas,id));
+    }
+    alertify.notify('Se cargo correctamente', 'success', 5, function(){  console.log('dismissed'); });
+}
+
+//esta funcion llama al framework llamado MailJs y sirve para mandar mails 
+function EnviarEmail() {
+    if(alumnosArray.getLargo()==0 )
+        return alertify.notify('No hay alumnos para enviar', 'error', 5, function(){  console.log('dismissed'); });
+
+    if(document.getElementById("mailAlumnoCampo").value == "")
+        return alertify.notify('No se completo la casilla "Ingresar mail"', 'error', 5, function(){  console.log('dismissed'); });
+
+    emailjs.send("service_8vmyxzh","template_1mxzo6p",{
+        from_name: "Alvaro Elena",
+        to_name: "User",
+        message: MostrarAlumnos(),
+        send_to: document.getElementById("mailAlumnoCampo").value,
+        })
+        .then(() => {
+            alertify.notify('Mail enviado correctamente', 'success', 5, function(){  console.log('dismissed'); });
+        }, (err) => {
+            alertify.notify('Error Enviando el Mail' , 'error', 5, function(){  console.log(err); });
+    });
+}
+
+function DescargarAlumnos() {
+    if(alumnosArray.getLargo()==0)
+    {
+        alertify.notify('No hay alumnos para guardar', 'error', 5, function(){  console.log('dismissed'); });
+    }
+    else
+    {
+        const jsonTest = MostrarAlumnos();
+        var a = document.createElement("a");
+        var file = new Blob([jsonTest]);
+        a.href = URL.createObjectURL(file);
+        a.download = "inventory.json";
+        a.click();
+        alertify.notify('Los Alumnos se descargaron correctamente', 'success', 5, function(){  console.log('dismissed'); });
+    }
+  
+}
+
+function EditarAlumno()
+{
+    if(alumnosArray.getLargo()==0 )
+    return alertify.notify('No hay alumnos cargados', 'error', 5, function(){  console.log('dismissed'); });
+
+    let alumnoAux = alumnosArray.getIndex(parseInt(document.getElementById("idAlumnoCampo").value));
+    if(alumnoAux>=0)
+    {
+        alertify.confirm('Desea editar este alumno?', alumnosArray.lista[alumnoAux].getAlumno(), 
+        function()
+        { 
+            document.getElementById("editarAlumno").style.display  = "none";
+            document.getElementById("editarAlumnoConfirmacion").style.display  = "inline";
+            ActivarCamposAgregarAlumno();
+            document.getElementById("inputText2").value =alumnosArray.lista[alumnoAux].getNota() ;
+            document.getElementById("inputText").value= alumnosArray.lista[alumnoAux].getNombre() ;
+            document.getElementById("inputText1").value= alumnosArray.lista[alumnoAux].getApellido() ;
+
+        }, 
+        function(){ alertify.error('Se cancelo la eliminacion del usuario')});
+    }else
+    {
+        alertify.alert('Error','Id ingresado no corresponde al alumno');
+    }
+}
+
+function editarAlumnoConfirmacion()
+{   
+    let alumnoAux = alumnosArray.getIndex(parseInt(document.getElementById("idAlumnoCampo").value));
+    if(alumnoAux>=0)
+    {
+        alertify.confirm('Cambiar valores del alumno?', 
+        function()
+        { 
+            alumnosArray.lista[alumnoAux].setNota(parseInt(document.getElementById("inputText2").value)) ;
+            alumnosArray.lista[alumnoAux].setNombre(document.getElementById("inputText").value) ;
+            alumnosArray.lista[alumnoAux].setApellido(document.getElementById("inputText1").value) ;
+            MostrarElementos(1);
+            alertify.success('Usuario Editado');
+        }, 
+        function(){ alertify.error('Se cancelo la Editacion del usuario')});
+    }else
+    {
+        alertify.alert('Error','Id ingresado no corresponde al alumno');
+    }
+
+
+}
+
+function EliminarAlumno()
+{
+    if(alumnosArray.getLargo()==0 )
+        return alertify.notify('No hay alumnos cargados', 'error', 5, function(){  console.log('dismissed'); });
+
+        let alumnoAux = alumnosArray.getIndex(parseInt(document.getElementById("idAlumnoCampo").value));
+        if(alumnoAux>=0)
+        {
+            alertify.confirm('Desea Eliminar este alumno?', alumnosArray.lista[alumnoAux].getAlumno(), 
+            function()
+            { 
+                alumnosArray.eliminarAlumno(alumnoAux);
+                alertify.success('Usuario eliminado');
+            }, 
+            function(){ alertify.error('Se cancelo la eliminacion del usuario')});
+        }else
+        {
+            alertify.alert('Error','Id ingresado no corresponde al alumno');
+        }
+}
+
+
+function PaginaCargar()
+{
+    MostrarElementos(0);
+    document.getElementById("TituloPagina").textContent = "Cargar Usuarios";
+
+}
+function PaginaEditar()
+{
+    MostrarElementos(1);
+    document.getElementById("TituloPagina").textContent = "Editar Usuarios";
+}
+
+function PaginaEliminar()
+{   
+    MostrarElementos(2);
+    document.getElementById("TituloPagina").textContent = "Eliminar Usuarios";
+}
+
+function PaginaEnviar()
+{
+    MostrarElementos(3);
+    document.getElementById("TituloPagina").textContent = "Enviar Usuarios";
+}
+
+function PaginaOtros()
+{
+    MostrarElementos(4);
+    document.getElementById("TituloPagina").textContent = "Otros";
+}
+
+function MostrarElementos(dato)
+{
+    LimpiarCampos();
+    document.getElementById("editarAlumno").style.display = "none";
+    document.getElementById("agregarAlumnos").style.display = "none";
+    document.getElementById("bestAlumno").style.display = "none";
+    document.getElementById("worstAlumno").style.display = "none";
+    document.getElementById("prodNotas").style.display = "none";
+    document.getElementById("guardarAlumnos").style.display = "none";
+    document.getElementById("cargarAlumnos").style.display = "none";
+    document.getElementById("hardcodearAlumnos").style.display = "none";
+    document.getElementById("enviarMail").style.display = "none";
+    document.getElementById("descargarUsuario").style.display = "none";
+    document.getElementById("idAlumnoCampo").style.display  = "none";
+    document.getElementById("mailAlumnoCampo").style.display  = "none";
+    document.getElementById("EliminarAlumnos").style.display  = "none";
+
+    document.getElementById("editarAlumnoConfirmacion").style.display  = "none";
+
+    document.getElementById("mailAlumnoTexto").textContent = "";
+    document.getElementById("idAlumnoTexto").textContent = "";
+
+    document.getElementById("inputText").style.display  = "none";
+    document.getElementById("inputText1").style.display  = "none";
+    document.getElementById("inputText2").style.display  = "none";
+
+    document.getElementById("Text").textContent = "";
+    document.getElementById("Text1").textContent = "";
+    document.getElementById("Text2").textContent = "";
+    
+    if(dato==0) //Cargar Alumnos
+    {
+        
+        ActivarCamposAgregarAlumno();
+        
+        document.getElementById("agregarAlumnos").style.display = "inline";
+        document.getElementById("cargarAlumnos").style.display = "inline"; 
+        document.getElementById("hardcodearAlumnos").style.display = "inline";   
+
+    }else if(dato==1) //Editar Alumnos
+    {
+        document.getElementById("editarAlumno").style.display = "inline";
+        document.getElementById("idAlumnoTexto").textContent = "Ingrese id del alumno";
+        document.getElementById("idAlumnoCampo").style.display  = "inline";
+
+    }else if(dato==2) //Eliminar Alumnos
+    {
+        document.getElementById("EliminarAlumnos").style.display  = "inline";
+        document.getElementById("idAlumnoCampo").style.display  = "inline";
+        document.getElementById("idAlumnoTexto").textContent = "Ingrese id del alumno";
+        
+    }else if(dato==3)//Enviar/guardar Alumnos
+    {
+        document.getElementById("guardarAlumnos").style.display = "inline";
+        document.getElementById("enviarMail").style.display = "inline";
+        document.getElementById("descargarUsuario").style.display = "inline";
+
+        document.getElementById("mailAlumnoTexto").textContent = "Ingresar Mail para enviar";
+        document.getElementById("mailAlumnoCampo").style.display  = "inline";
+    }else if(dato==4)//otros
+    {
+        document.getElementById("bestAlumno").style.display = "inline";
+        document.getElementById("worstAlumno").style.display = "inline";
+        document.getElementById("prodNotas").style.display = "inline";
+    }
+}
+
+
+
+function ActivarCamposAgregarAlumno()
+{
+    document.getElementById("Text").textContent = "Ingrese Nombre del alumno";
+    document.getElementById("Text1").textContent = "Ingrese apellido del alumno";
+    document.getElementById("Text2").textContent = "Ingrese nota del alumno";
+    
+    document.getElementById("inputText").style.display  = "inline";
+    document.getElementById("inputText1").style.display  = "inline";
+    document.getElementById("inputText2").style.display  = "inline"; 
+}
